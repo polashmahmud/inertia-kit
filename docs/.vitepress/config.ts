1ea912196d5f953b0a81907e1345eacf11b0afda
Polash Mahmud <polashmahmud@gmail.com>
2025-11-07 19:09:01 +0600

    feat: add GitHub Actions workflow for deploying VitePress site to GitHub Pages

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "Vue UI Kit",
   description: "A VitePress Site",
+  base: "/inertia-kit/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
